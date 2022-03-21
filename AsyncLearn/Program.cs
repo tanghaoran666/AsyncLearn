af56c9b5899e5e64645d4dd1e69e1c10e1722318
haoran.tang <haoran.tang@thoughtworks.com>
2022-03-21 21:01:57 +0800

    task.whenAny

--- AsyncLearn/Program.cs
+++ AsyncLearn/Program.cs
@@ -19,10 +19,24 @@ namespace asyncLearn
             Juice oj = PourOJ();
             Console.WriteLine("oj is ready");
             
-            await Task.WhenAll(eggsTask, baconTask, toastTask);
-            Console.WriteLine("Eggs are ready");
-            Console.WriteLine("Bacon is ready");
-            Console.WriteLine("Toast is ready");
+            var breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
+            while (breakfastTasks.Count > 0)
+            {
+                Task finishedTask = await Task.WhenAny(breakfastTasks);
+                if (finishedTask == eggsTask)
+                {
+                    Console.WriteLine("Eggs are ready");
+                }
+                else if (finishedTask == baconTask)
+                {
+                    Console.WriteLine("Bacon is ready");
+                }
+                else if (finishedTask == toastTask)
+                {
+                    Console.WriteLine("Toast is ready");
+                }
+                breakfastTasks.Remove(finishedTask);
+            }
             Console.WriteLine("Breakfast is ready!");
             
             stopwatch.Stop();
